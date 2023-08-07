ba92aa62a2c4617e765fab3a25ba5fb1c8a1b2c2
unknown <thahira.f2021@gmail.com>
2023-08-07 12:50:06 -0400

    first commit

---
+++ src/Components/Meal.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Meal= ()=> {
+  return (
+    <>
+    <div className="main">
+        <div className="heading">
+            <h1>Search Your Favourite Recipes Here!</h1>
+            <h3> "The Secret Ingerident Is Cheese"</h3>
+            <h4>People who give their food, give you their heart</h4>
+
+        </div>
+        <div className="searchBox">
+            <input type="search" className="search-bar"/>
+
+        </div>
+
+    </div>
+    </>
+  )
+}
+
+export default Meal
