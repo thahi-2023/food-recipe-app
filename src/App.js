0e6acbe07873a6551cb7ae673b257f35f3eae5ee
unknown <thahira.f2021@gmail.com>
2023-08-09 07:20:29 -0400

    getting Recipe Info

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Meal from './Components/Meal'
 import './Components/style.css';
+import {Routes, Route} from "react-router-dom";
+
 
 function App() {
   return (
     <>
-    <Meal/>
+    <Routes>
+     <Route path="/" element={<Meal/>}/>
+    </Routes>
     </>
       
       
---
+++ src/Components/RecipeInfo.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const RecipeInfo=()=>{
+    return(
+        <>
+
+        </>
+    )
+}
+export default RecipeInfo;
