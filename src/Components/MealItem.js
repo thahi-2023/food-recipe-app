55c9df42b41f77fd84c59723741fcd1781875f3b
unknown <thahira.f2021@gmail.com>
2023-08-11 12:15:29 -0400

    set path for image

--- src/Components/MealItem.js
+++ src/Components/MealItem.js
@@ -10,6 +10,7 @@ const MealItem =({data})=> {
         <>
         {
             (!data) ? "Recipe Not Found": data.map(item=>{
+                //map to iterate over an array
                 return (
                     <div className="card" key={item.idMeal} onClick={()=>{navigate(`/${item.idMeal}`)}}>
           <img
